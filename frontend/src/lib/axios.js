7889c97c84b820f90e605a378be3c69a76bf62f0
chmcs2026arpityadav-wq <chmcs2026.arpit.yadav@gmail.com>
2026-03-06 14:14:19 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3000", // your backend URL
-  baseURL:https://mern-moviecollection.onrender.com
+  baseURL:"https://moviecollection-u34e.onrender.com"
 });
 
 export default api;
